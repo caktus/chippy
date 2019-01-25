245280851b50b97bd28b5710660b6fb37b41d600
nmashton <neil.m.ashton@gmail.com>
2019-01-25 10:13:22 -0500

    Actually fetch the sprint

--- lib/chippy_web/channels/sprint_channel.ex
+++ lib/chippy_web/channels/sprint_channel.ex
@@ -1,7 +1,15 @@
 defmodule ChippyWeb.SprintChannel do
   use Phoenix.Channel
 
-  def join("sprint:" <> _sid, _message, socket) do
-    {:ok, socket}
+  alias Chippy.SprintServer
+
+  def join("sprint:" <> sid, _message, socket) do
+    case SprintServer.sprint_pid(sid) do
+      pid when is_pid(pid) ->
+        {:ok, socket}
+
+      nil ->
+        {:error, "Sprint not found"}
+    end
   end
 end
