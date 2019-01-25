5689b408f98dad4087bb5983bc26cfdf27037119
nmashton <neil.m.ashton@gmail.com>
2019-01-25 10:20:48 -0500

    Send over "by_users" after you join

--- lib/chippy_web/channels/sprint_channel.ex
+++ lib/chippy_web/channels/sprint_channel.ex
@@ -6,10 +6,19 @@ defmodule ChippyWeb.SprintChannel do
   def join("sprint:" <> sid, _message, socket) do
     case SprintServer.sprint_pid(sid) do
       pid when is_pid(pid) ->
+        send(self(), {:joined, sid})
         {:ok, socket}
 
       nil ->
         {:error, "Sprint not found"}
     end
+  end
+
+  def handle_info({:joined, sid}, socket) do
+    by_users = SprintServer.display_by_users(sid)
+
+    push(socket, "by_users", by_users)
+
+    {:noreply, socket}
   end
 end
