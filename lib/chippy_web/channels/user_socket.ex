0add11a0ce7d9a8aa4f5d56919500bc581fa61d5
Tobias McNulty <tobias@caktusgroup.com>
2019-04-12 13:54:46 -0400

    delete SprintChannel

--- lib/chippy_web/channels/user_socket.ex
+++ lib/chippy_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ChippyWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  channel "sprint:*", ChippyWeb.SprintChannel
+  # channel "sprint:*", ChippyWeb.SprintChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
