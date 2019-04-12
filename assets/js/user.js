2e65b8c24f9573de4cab1b774bcb97fc317c91a5
Tobias McNulty <tobias@caktusgroup.com>
2019-04-12 10:46:29 -0400

    allow user to set their own name

--- assets/js/user.js
+++ assets/js/user.js
@@ -4,8 +4,7 @@ import RandomNames from "random-human-name";
 export const getUserName = () => {
   var name = getCookie('user_id');
   if (!name) {
-    // FIXME: set back to uuidv4() when we implement name customization
-    name = RandomNames.RandomNames(1)[0];
+    name = prompt("Please enter your name", RandomNames.RandomNames(1)[0]);
     setCookie('user_id', name);
     document.getElementById('user_id').innerHTML = name;
   }
