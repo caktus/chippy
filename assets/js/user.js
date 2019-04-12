90dee61920e3c8a7d5ef6459486fe872ea2d6877
nmashton <neil.m.ashton@gmail.com>
2019-04-12 10:53:40 -0400

    Add user_color

--- assets/js/user.js
+++ assets/js/user.js
@@ -3,10 +3,15 @@ import RandomNames from "random-human-name";
 
 export const getUserName = () => {
   var name = getCookie('user_id');
+  var color = getCookie('user_color');
   if (!name) {
     name = prompt("Please enter your name", RandomNames.RandomNames(1)[0]);
     setCookie('user_id', name);
     document.getElementById('user_id').innerHTML = name;
+  }
+  if (!color) {
+    color = prompt("Please choose your favorite color");
+    setCookie("user_color", color);
   }
   return name;
 }
